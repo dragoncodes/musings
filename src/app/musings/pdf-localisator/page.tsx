89257167bdaa923f68cab6ba6e391029a8aa4dd4
Petar Marinov <petar.marinov@trading212.com>
2023-11-13 23:48:42 +0200

    Fixed build errors

--- src/app/musings/pdf-localisator/page.tsx
+++ src/app/musings/pdf-localisator/page.tsx
@@ -1,4 +1,4 @@
-import { execFile, execSync } from "child_process";
+import { execFile } from "child_process";
 import fs from "fs";
 import { redirect } from "next/navigation";
 import { FileLike } from "openai/uploads";
@@ -11,55 +11,54 @@ const binaryPath = path.join(binPath, "pdf-localisator");
 
 process.env.PATH = `${binPath}:${envPath}`;
 
-export default async function PdfLocalisator() {
-  async function onSubmit(form: FormData) {
-    "use server";
+async function onSubmit(form: FormData) {
+  "use server";
 
-    const file = form.get("file") as FileLike;
+  const file = form.get("file") as FileLike;
 
-    const tempPath = path.join(process.cwd(), "temp.pdf");
+  const tempPath = path.join(process.cwd(), "temp.pdf");
 
-    const b = Buffer.from(await file.text());
+  const b = Buffer.from(await file.text());
 
-    fs.createWriteStream(tempPath).write(b);
+  fs.createWriteStream(tempPath).write(b);
 
-    console.log("tempPath", tempPath, "saved");
+  console.log("tempPath", tempPath, "saved");
 
-    const fileName = await new Promise<string>((resolve, reject) => {
-      execFile(
-        binaryPath,
-        [tempPath, "bulgarian"],
-        {
-          cwd: path.dirname(binaryPath),
-          env: {
-            ...process.env,
-          },
+  const fileName = await new Promise<string>((resolve, reject) => {
+    execFile(
+      binaryPath,
+      [tempPath, "bulgarian"],
+      {
+        cwd: path.dirname(binaryPath),
+        env: {
+          ...process.env,
         },
-        (error, stdout, stderr) => {
-          if (error) {
-            // Handle the error
-            console.error("Error:", error);
-            reject();
-          } else {
-            const resultFileName = file.name.replace(".pdf", ".txt");
+      },
+      (error, stdout) => {
+        if (error) {
+          console.error("Error:", error);
+          reject();
+        } else {
+          const resultFileName = file.name.replace(".pdf", ".txt");
 
-            fs.writeFileSync(
-              path.join(process.cwd(), "public", resultFileName),
-              stdout,
-            );
+          fs.writeFileSync(
+            path.join(process.cwd(), "public", resultFileName),
+            stdout,
+          );
 
-            resolve(resultFileName);
-          }
-        },
-      );
-    });
+          resolve(resultFileName);
+        }
+      },
+    );
+  });
 
-    redirect(`/${fileName}`);
-  }
+  redirect(`/${fileName}`);
+}
 
+export default function PdfLocalisator() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-      <form action={onSubmit}>
+      <form action={void onSubmit}>
         <input type="file" name="file" />
 
         <button type="submit"> Submit</button>
